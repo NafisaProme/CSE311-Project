6e8c75062df7fbb5daa8ad3439f861a981d308a6
RifatXia <rifatxia9@gmail.com>
2021-12-14 05:35:24 +0600

    init

---
+++ user_class.js
@@ -0,0 +1,21 @@
+class User
+{
+    // the constructor to set the values 
+    constructor(mail, name)
+    {
+        // variables are decalred and set here 
+        this.mail = mail;
+        this.name = name;
+    }
+
+    // method declaration 
+    login()
+    {
+        console.log(this.name + ' has logged in\n');
+    }
+}
+
+let user = new User('Zia@gmail.com', 'Zia');
+
+console.log(user.mail);
+user.login();
